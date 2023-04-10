16991e8731be814bbba94eeb30c2799b0ea96e5c
jayhust <jayhust@163.com>
2023-04-10 11:19:24 +0800

    second commit

--- test/ERC20Token.test.js
+++ test/ERC20Token.test.js
@@ -15,7 +15,8 @@ contract("ERC20Token", accounts => {
       let user = accounts[2]; // eth address
       assert.equal(user, "0x88eBF600Ab3cb4714404315e633EB90da582F51C");
       let mint_amount = 127;
-      await instance.mint.call(user, mint_amount); // mint
+      // await instance.mint.call(user, mint_amount); // wrong usage!!! won't change the balance!!!
+      await instance.mint(user, mint_amount); // mint
       const user_balance = await instance.balanceOf.call(user); // get the user balance
       const total_supply = await instance.totalSupply.call(); // get the total supply
       console.log('contract total supply: ' + total_supply); // log out the total supply
